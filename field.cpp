e27eca852b426591f6efe840956beac9c7a12b42
prijutme4ty <prijutme4ty@gmail.com>
2014-12-04 11:31:29 +0300

    check mate

--- field.cpp
+++ field.cpp
@@ -71,7 +71,7 @@ public:
     //   v_field[m_letter][m_digit] = figure;
     //   return;
     // }
-    // throw std::logic_error ("Not allowed co-ordinates"); 
+    // throw std::logic_error ("Not allowed co-ordinates");
   }
 
   void setFigure(const Coordinates& coord, Figure* figure) {
@@ -91,7 +91,7 @@ public:
     return *v_field[coord.column()][coord.row()];
   }
 
-  
+
   Coordinates king_position (Color current_side) {
     for (int i = 0; i<=7; ++i) {
       for (int k = 0; k<=7; ++k) {
@@ -102,7 +102,7 @@ public:
     }
   }
 
-  bool check_check (Color current_side) { // проверка, поставлен ли шах игроку переданного цвета 
+  bool check_check (Color current_side) { // проверка, поставлен ли шах игроку переданного цвета
 
     Coordinates king_position = king_position(current_side); // this line is
     // Coordinates king_position(king_position(current_side)); // equivalent
@@ -113,7 +113,6 @@ public:
 			//*v_field[i][k].check_eat?
 			  if (get_figure(i,k).type() != KING && get_figure(i,k).type() != EMPTY_CELL && get_figure(i,k).colour() != current_side) {
           EatTurn t  = EatTurn( Coordinates(i,k), king_position );
-				  
 				  if (get_figure(i,k).check_eat(&t)) {
 						return true;
 				  }
@@ -123,11 +122,49 @@ public:
 	  return false;
   }
 
+  vector<Coordinates> coords_assault_on_king_figs (Color current_side) { // возвращение вектора координат фигур, ставящих шах
+
+    vector<Coordinates> v_coords_assault_on_king_figs = vector<Coordinates>();
+
+    Coordinates king_position = king_position(current_side);
+	  for (int i = 0; i<=7; ++i) {
+		  for (int k = 0; k<=7; ++k) {
+			  if (get_figure(i,k).type() != KING && get_figure(i,k).type() != EMPTY_CELL && get_figure(i,k).colour() != current_side) {
+          EatTurn t  = EatTurn( Coordinates(i,k), king_position );
+				
+				  if (get_figure(i,k).check_eat(&t)) {
+						Coordinates assault_on_king_figure = Coordinates(i,k);
+						v_coords_assault_on_king_figs.push_back(assault_on_king_figure);
+				  }
+			  }
+		  }
+	  }
+	  return v_coords_assault_on_king_figs;
+  }
+
+
+  vector<Coordinates> king_possibilities (Color current_side) {
+	  vector<Coordinates> vector_king_possibilities;
+	  Coordinates center_coord = king_position(current_side);
+	  center_row = center_coord.row();
+	  center_col = center_coord.column();
+	for (int i = center_row - 1; i <= center_row + 1; ++i ) {
+		for (int k = center_col - 1; k <= center_col + 1; ++k) {
+			if (i != k {
+				Coordinates el_vector_k_p = Coordinates(i,k);
+				vektor_king_possibilities.push_back(el_vector_k_p);
+			}
+		}
+	}
+	return vector_king_possibilities;
+  }
+
   bool check_mate (Color current_side) {
     if (!check_check(current_side)) {
         return false;
     }
     // vector<AbstractTurn*> king_possibilities_2(...)
+
     vector<Coordinates> vector_king_possibilities = king_possibilities(current_side);
     for (int i = 0; i < vector_king_possibilities.size(); ++i) {
         if (get_figure(vector_king_possibilities[i]).type() == EMPTY_CELL) {
@@ -141,10 +178,60 @@ public:
           }
 
         } // else { ...eat turn... }
+		else {
+      EatTurn t(king_position(current_side), vector_king_possibilities[i]);
+      if (t.check(*this)) {
+        Field field_copy = *this;
+        t.apply(field_copy);
+        if (!field_copy.check_check(current_side)) {
+          return false;
+        }
+      }
     }
 
+
+
     // проверка загораживания
-    
+  	if (coords_assault_on_king_figs.size() == 2) {
+  		return true;
+  	} else {
+  		Coordinates coord_assault = coords_assault_on_king_figs[0];
+  		EatTurn e_t = EatTurn(coord_assault, king_position(current_side));
+  		vector<Coordinates> v_for_check_defense =  get_figure(coord_assault).path(*e_t);
+  		for (int i = 0; i<=7; ++i) {
+  		  for (int k = 0; k<=7; ++k) {
+  				if (get_figure(i,k).type() != EMPTY_CELL && get_figure(i,k).colour() == current_side) {
+  					for (int counter = 0; counter < v_for_check_defence.size(); ++counter) {
+  						NonEatTurn n_e_t = NonEatTurn(Coordinates(i,k), v_for_check_defence[counter]);
+  						if (n_e_t.check(*this)) {
+  							Field field_copy = *this;
+  							n_e_t.apply(field_copy);
+  							if (!field_copy.check_check(current_side)) {
+    							return false;
+  							}
+  						}
+  					}
+  				}
+  		  }
+  		}
+  	}
+
+  	//проверка съедения атакующей фигуры
+  	Coordinates coord_assault = coords_assault_on_king_figs[0];
+    for (int i = 0; i<=7; ++i) {
+  	  for (int k = 0; k<=7; ++k) {
+  			if (get_figure(i,k).type() != EMPTY_CELL && get_figure(i,k).colour() == current_side) {
+  				EatTurn e_t = EatTurn(Coordinates(i,k), coord_assault);
+  				if (e_t.check(*this)) {
+  					Field field_copy = *this;
+  					e_t.apply(field_copy);
+  					if (!field_copy.check_check(current_side)) {
+  						return false;
+  					}
+  				}
+    		}
+  		}
+    }
     return true;
   }
 
